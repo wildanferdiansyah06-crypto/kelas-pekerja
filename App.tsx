08197f39eb98390eb7bb1b8af6639112d591682b
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-05 23:33:31 +0800

    Remove Rak route from App component
    Removed the Rak route from the App component.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Home";
-import Rak from "./Rak";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-
-        {/* Homepage */}
         <Route path="/" element={<HomePage />} />
-
-        {/* Halaman Rak Buku */}
-        <Route path="/rak" element={<Rak />} />
-
       </Routes>
     </Router>
   );
