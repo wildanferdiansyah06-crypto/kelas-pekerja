8000e84c4319bcc9df664b72c2a55858a984c35e
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-06 01:28:40 +0800

    Update theme.ts

--- theme.ts
+++ theme.ts
@@ -1,7 +1,22 @@
 export function getTheme() {
-return localStorage.getItem("theme") === "dark";
+const saved = localStorage.getItem("theme");
+return saved === "dark";
 }
 
 export function setTheme(isDark: boolean) {
 localStorage.setItem("theme", isDark ? "dark" : "light");
+
+if (isDark) {
+document.documentElement.classList.add("dark");
+} else {
+document.documentElement.classList.remove("dark");
+}
+}
+
+export function initTheme() {
+const saved = localStorage.getItem("theme");
+
+if (saved === "dark") {
+document.documentElement.classList.add("dark");
+}
 }
