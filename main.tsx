525687c133a3be339d3d5dd9751cf2490ca35570
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-06 10:28:41 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 import "./index.css";
 
 import { initTheme } from "./theme";
 
-// Apply theme sebelum React render
 initTheme();
 
 createRoot(document.getElementById("root")!).render(
