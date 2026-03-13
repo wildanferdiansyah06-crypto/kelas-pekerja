bb0f9b00e835bc11de624e71fc39b2c82ad76465
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-13 17:27:11 +0800

    Update LayoutWrapper.tsx

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -1,37 +1,13 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import Navbar from "@/src/components/Navbar";
-import Footer from "@/src/components/Footer";
-
 export default function LayoutWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  // pecah path jadi segment
-  const segments = pathname.split("/").filter(Boolean);
-
-  /**
-   * Reader mode hanya untuk:
-   * /buku/[slug]
-   */
-  const isBookReader =
-    segments.length === 2 && segments[0] === "buku";
-
   return (
     <>
-      {/* Navbar */}
-      {!isBookReader && <Navbar />}
-
-      <main className={`min-h-screen ${!isBookReader ? "pt-16" : ""}`}>
-        {children}
-      </main>
-
-      {/* Footer */}
-      {!isBookReader && <Footer />}
+      {children}
     </>
   );
 }
