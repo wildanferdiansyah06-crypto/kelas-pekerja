92a5b40ce2dad9c578c14bebd3f8c2db0533c9df
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-03-14 15:56:02 +0800

    Update layout.tsx

--- src/app/buku/layout.tsx
+++ src/app/buku/layout.tsx
@@ -1,6 +1,5 @@
 import ReadingProgress from "@/src/components/ReadingProgress";
 import ScrollToTop from "@/src/components/ScrollToTop";
-import Footer from "@/src/components/Footer";
 
 export default function BukuLayout({
   children,
@@ -9,12 +8,12 @@ export default function BukuLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-
+      
       {/* Progress bar membaca */}
       <ReadingProgress />
 
       {/* Konten buku */}
-      <main className="flex-1 max-w-3xl mx-auto px-6 pt-24 pb-32">
+      <main className="flex-1 max-w-5xl mx-auto px-6 pt-24 pb-32">
         {children}
       </main>
 
