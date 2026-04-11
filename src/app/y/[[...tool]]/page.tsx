1b9ebeec45c04ca040ce822162a56ea4ac40f24f
wildanferdiansyah06-crypto <wildanferdiansyah06@gmail.com>
2026-04-11 20:30:29 +0800

    Remove Studio page due to dependency conflicts

--- src/app/y/[[...tool]]/page.tsx
+++ src/app/y/[[...tool]]/page.tsx
@@ -1,3 +1,3 @@
-import config from '../../../../sanity.config'
-
-export default config
+// Studio page removed due to dependency conflicts with Next.js 15
+// Use hosted Sanity Studio at: https://www.sanity.io/manage/project/frlqeeaf/studio
+// To deploy schema changes, run: npx sanity deploy
